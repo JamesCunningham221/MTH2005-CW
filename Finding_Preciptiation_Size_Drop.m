9befa881635c2512aa51d42fd242717f91fedeee
shannonwilliamss <114595603+shannonwilliamss@users.noreply.github.com>
2024-03-16 16:15:58 +0000

    Update Finding_Preciptiation_Size_Drop.m

--- Finding_Preciptiation_Size_Drop.m
+++ Finding_Preciptiation_Size_Drop.m
@@ -17,31 +17,10 @@ Kv = 2.21e-5 ;                    %! Diffusivity of Water Vapour (m^2 s^-1)
 
 s = 0.003;        % constant supersaturation percentage
 T = 282;          % constant temperature in Kelvin
-
-% Set timestep and number of steps to iterate
-dt = 0.1;                % timestep
-n_steps = 250*60;     % number of steps based on timestep
-t = 0:dt:n_steps;           % time up to 2700 seconds (45 minutes)
-r = 1e-6:(100e-6 - 1e-6)/n_steps:100e-6;   % initial radius in microns
-
+r(1) = 1e-6;      % initial radius in metres
 
 % Constant A3 from Davenish et al.
 A3 = ((Lv^2 * Rho_w)/(k*Rv*T^2) + (Rho_w * Rv * T)/(Kv*svp(T)))^(-1);
 
-% Calculate the true solution
-c = 0.5*r(1)^2;
-rstar = (2*A3*s*t + 2*c).^(1/2);
-
-% Plot true solution
-plot(rstar*10^(6), t/60)
-
-ylabel('Time [minutes]') 
-xlabel('Droplet Radius [microns, \mum]')
-title({'True Solution for the Growth of the';'Droplet Radius Over 45 Minutes'}) 
-subtitle('for Temperature 282K and Initial Radius 1 [\mum]')
-grid on
-
-r_star = 100e-6;
-g = (r_star^2 - 2*c)/(2*A3*s);
-
-g %in second!
+r_star = 0.5e-3;
+t = (r_star^2 - 2*c)/(2*A3*s) %! Time taken to grow (s)
